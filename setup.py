0d7a57d5b38b9d39a549992d3dcb414e2ed9f974
presider <fikret_yel@yahoo.com>
2017-04-10 09:34:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import sys
 from setuptools import setup, find_packages
 
 if sys.version_info < (3, 2):
-    raise Exception('Only Python 3.2+ is supported')
+    raise Exception('Sadece Python 3.2+  destekleniyor')
 
 setup(name='import_from_github_com',
       version='0.1',
-      description="Python module finder/loader from github, like in golang",
+      description="Python modullerini github'dan bulan ve yukleyen arac,  golang gibi",
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
       url='https://github.com/nvbn/import_from_github_com',
